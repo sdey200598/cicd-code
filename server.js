94a5f20ca6fee21877a9b5391cd0d33fc7144551
Swarup Dey <dey.swarup@thinkfuture.us>
2025-12-01 19:21:13 +0530

    Added Some Code

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node Js!');
+  res.send('Hello from Node Js! You are awesome!');
 });
 
-const PORT = 3000;
+const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
