c4f9cfd90743e3b741b0ba727381815b63accb2c
Swarup Dey <dey.swarup@thinkfuture.us>
2025-12-08 23:30:03 +0530

    Removed String

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+// Pipeline created successfully. From tomorrow, carefully revise the entire training and then upload the code to Google Drive.
+
 const express = require('express');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node Js. You are Great Coder Swarup Dey. You are doing awesome work!');
+  res.send('You are Great Coder Swarup Dey. You are doing awesome work!');
 });
 
 const PORT = 8000;
